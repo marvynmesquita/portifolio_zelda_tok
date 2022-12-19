6a787837534d0765266a6932f3aa08a990ebe184
marvynmesquita <31486769+marvynmesquita@users.noreply.github.com>
2022-12-19 14:16:29 -0300

    added fade effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+AOS.init({
+    duration: 1000,
+    easing: 'ease',
+});
+
 function trailer() {
     if (document.getElementById('trailer').classList.contains('show-trailer')) {
         document.getElementById('trailer').classList.remove('show-trailer');
